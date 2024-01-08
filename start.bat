fac1ade8bca1e492201a3a5b0b5ab1fe05e20e91
x10ss <jayoung27@icloud.com>
2024-01-08 15:17:15 -0500

    modified:   start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 
 git pull
-IF NOT EXIST config.yml ren config-default.yml config.yml
+IF NOT EXIST config.yml copy config-default.yml config.yml
 FOR /F %%i IN (dependencies.txt) DO @pip install %%i
 pause
 python SaladInfo.py
